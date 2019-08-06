7be8c9a776b71a190ac0830ef1a7bb234de57fb7
ettern <anderskarlsen@hotmail.com>
2019-08-06 09:33:59 +0200

    Added files

---
+++ Ettern_1-6_Durotar.lua
@@ -0,0 +1,81 @@
+Guidelime.registerGuide(
+[[
+[N1-6 Valley of Trials]
+[GA Orc,Troll]
+[D A guide created by |cFF69CCF0Ettern|r\\http://www.dreamstate.gg]
+[G43.3,68.5 Durotar]Accept [QA4641 Your Place In The World] in front of you.
+[G42.6,69]Accept [QA1485 Vile Familiars][A Warlock].
+[G42.1,68.3]Turn in [QT4641 Your Place In The World]. \\Accept [QA788 Cutting Teeth].
+Unequip leggings, boots and weapon then sell it together with Tough Jerky to [V]vendor[G40.6,67.8] so you have atleast 10 copper.[A Warlock]
+[G40.65,68.51][T]|cFF9482C9 Learn new skills:\\- Immolate (Rank 1)|r[A Warlock]. 
+[G40.6,62.6]Pickup [QA790 Sarkoth]. \\Kill Mottled Boars along your way for [QC788 Cutting Teeth][O]
+Kill Sarkoth[G40.5,67.3] for [QC790 Sarkoth] and loot him.
+[G40.6,62.6]Turn in [QT790 Sarkoth]. \\Accept [QA804 Sarkoth].
+Kill Mottled Boars and finish [QC788 Cutting Teeth].
+[G44.7,57]Do [QC1485 Vile Familiars][A Warlock].
+Grind mobs until you're [XP4-600 600 XP from level 4][A Warlock].
+Grind mobs until you're [XP3-250 250 XP from level 3][A Mage,Warrior,Hunter,Rogue,Shaman,Priest].
+[G42.6,69]Turn in [QT1485 Vile Familiars]. Accept [QA1499 Vile Familiars][A Warlock]
+[G42.8,69.1]Next to you, turn in [QT1499 Vile Familiars]. Accept [QA794 Burning Blade Medallion].[A Warlock]
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3086 Glyphic Tablet][A Troll, Mage].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3065 Simple Tablet][A Troll, Warrior].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3082 Etched Tablet][A Troll, Hunter].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3083 Encrypted Tablet][A Troll, Rogue].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3084 Rune-Inscribed Tablet][A Troll, Shaman].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3085 Hallowed Tablet][A Priest].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3088 Encrypted Parchment][A Orc, Rogue].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3087 Etched Parchment][A Orc, Hunter].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3089 Rune-Inscribed Parchment][A Orc, Shaman].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA2383 Simple Parchment][A Orc, Warrior].
+[G42.1,68.3]Turn in [QT788 Cutting Teeth] and [QT804 Sarkoth].\\Accept [QA789 Sting of the Scorpid] and [QA3090 Tainted Parchment][A Warlock].
+[G41.3,68]Go further into the cave and turn in [QT3083 Encrypted Tablet][A Troll, Rogue]. \\[T]Learn new skills.
+[G41.3,68]Go further into the cave and turn in [QT3088 Encrypted Parchment][A Orc, Rogue]. \\[T]Learn new skills.
+Go further into the cave and [V]sell items here[G40.6,67.8][A Warlock].
+[G40.65,68.51]Next to you, turn in [QT3090 Tainted Parchment][A Warlock]. \\|cFF9482C9[T]Learn new skills.\\ - Corruption (Rank 1)|r
+[G42.7,67.3]Accept [QA4402 Galgar's Cactus Apple Surprise].\\[V]Sell items here.
+[G42.5,69]Turn in [QT3086 Glyphic Tablet]. \\[T]Learn new skills:|cFF69CCF0\\- Arcane Intellect (Rank 1)|r[A Mage]
+[G42.4,68.8]Turn in [QT3085 Hallowed Tablet]. \\[T]Learn new skills.[A Troll, Priest]
+[G42.4,69]Turn in [QT3084 Rune-Inscribed Tablet][A Troll, Shaman]. \\[T]Learn new skills.
+[G42.4,69]Turn in [QT3089 Rune-Inscribed Parchment]. \\[T]Learn new skills.[A Orc, Shaman]
+[G42.8,69.1]Accept [QA792 Vile Familiars] next to the wagon.[A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+[G42.8,69.3]Turn in [QT3087 Etched Parchment]. \\[T]Learn new skills.[A Orc, Hunter]
+[G42.8,69.3]Turn in [QT3082 Etched Tablet] \\[T]Learn new skills.[A Troll, Hunter].
+[G42.9,69.4]Turn in [QT2383 Simple Parchment]. \\[T]Learn new skills.[A Orc, Warrior]
+[G42.9,69.4]Turn in [QT3065 Simple Tablet]. \\[T]Learn new skills.[A Troll, Warrior]
+[G44.63,68.67]Accept [QA5441 Lazy Peons].
+Focus on doing [QC5441 Lazy Peons](See icon[L 41.2,72.7][L 42.3,73.2][L 44.7,72.8][L 47.6,69.4][L 45.6,65.7][L 47.2,65.5][L 46.8,60.8][L 47.1,57.9][L 43.8,57.8][L 42.7,57.4][L 41.3,58.8][L 40.9,60.4][L 38.8,61.9] on map for locations) together with the following quests: \\[QC4402 Galgar's Cactus Apple Surprise][OC] - Pick up cactus apples from plants\\[QC789 Sting of the Scorpid][OC] - Kill Scorpids. \\Head back to town once Lazy Peons is done. You dont need to finish the other quests now.[A Warlock]
+[G44.63,68.67]Turn in [QT5441 Lazy Peons]. \\Accept [QA6394 Thazz'ril's Pick].[A Warlock]
+Do [QC4402 Galgar's Cactus Apple Surprise] and [QC789 Sting of the Scorpid] while heading to the cave up north[G44.7,57].[A Warlock]
+Enter the cave and pick up Thazz'ril's Pick[G43.7,53.8] for [QC6394 Thazz'ril's Pick].[A Warlock]
+Go further into the cave and kill Yarrog Baneshadow[G42.7,53] for [QC794 Burning Blade Medallion].[A Warlock]
+Run back/Hearthstone/unstuck/die on purpose - so you end up in starting-town.[OC][A Warlock]
+From now focus on [QC5441 Lazy Peons] (See icon[L 41.2,72.7][L 42.3,73.2][L 44.7,72.8][L 47.6,69.4][L 45.6,65.7][L 47.2,65.5][L 46.8,60.8][L 47.1,57.9][L 43.8,57.8][L 42.7,57.4][L 41.3,58.8][L 40.9,60.4][L 38.8,61.9] on map for locations) together with: \\[QC4402 Galgar's Cactus Apple Surprise] - Pick up cactus apples from plants\\[QC789 Sting of the Scorpid] - Kill Scorpids.[A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+[G45.3,57.2]Kill Vile Familiars for [QC792 Vile Familiars][A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+Grind mobs until you're [XP4-1000 1000 XP from level 4][A Mage,Warrior,Hunter,Rogue,Shaman,Priest].
+Run back/Hearthstone/unstuck/die on purpose(outside of the cave) - so you end up in starting-town.[OC][A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+[G44.63,68.67]Turn in [QT5441 Lazy Peons].\\Accept [QA6394 Thazz'ril's Pick].[A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+[G42.7,67.3]Turn in [QT4402 Galgar's Cactus Apple Surprise].\\[V]Sell items here.
+[G42.1,68.3]Turn in [QT789 Sting of the Scorpid].
+[G42.4,69][T]Learn new skills.[A Shaman]
+[G42.4,68.8][T]Learn new skills.[A Priest]
+[G42.5,69][T]Learn new skills.|cFF69CCF0\\- Conjure Water\\- Frostbolt|r[A Mage]
+Next to you, accept [QA1516 Call of Earth (part 1)][G42.4,69.2][A Shaman].
+[G42.8,69.1]Turn in [QT792 Vile Familiars]. \\Accept [QA794 Burning Blade Medallion].[A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+[G42.8,69.3][T]Learn new skills.[A Hunter]
+[G42.9,69.4][T]Learn new skills.[A Warrior]
+Enter the cave up north and pick up Thazz'ril's Pick[G43.7,53.8] for [QC6394 Thazz'ril's Pick].[A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+Go further into the cave and kill Yarrog Baneshadow[G42.7,53] for [QC794 Burning Blade Medallion]. \\He is located at the end of the cave.[A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+Kill felstalkers inside the cave for [QC1516 Call of Earth (part 1)][A Shaman].
+Run back/Hearthstone/unstuck/die on purpose - so you end up in starting-town.[OC][A Mage,Warrior,Hunter,Rogue,Shaman,Priest]
+[G42.8,69.1]Turn in [QT794 Burning Blade Medallion]. \\Accept [QA805 Report to Sen'jin Village]
+[G42.4,69.2]Turn in [QT1516 Call of Earth (part 1)]. \\Accept [QA1517 Call of Earth (part 2)][A Shaman]
+[G44.63,68.67]Turn in [QT6394 Thazz'ril's Pick]
+Go to [G 41.7,73.2], then go up the hill to[G 40.7,74.2]. Turn left and continue to follow the path up [G 42.1,74.8] until you see a big stone[G 44,76.2].[A Shaman]
+Open your inventory and use the provided item, Earth Sapta. \\Turn in [QT1517 Call of Earth (part 2)]. \\Accept [QA1518 Call of Earth (part 3)][A Shaman]
+Run back/Hearthstone/unstuck - so you end up in starting-town.[OC][A Shaman]
+[G42.4,69.2]Turn in [QT1518 Call of Earth (part 3)][A Shaman]
+Leave starting zone and pick up [QT2161 A Peon's Burden][G52.1,68.3].
+Grind mobs around Sen'jin Village until level [XP6]
+[NX 6-12 Durotar]
+]], "Ettern leveling guide")
+
---
+++ Ettern_6-12_Durotar.lua
@@ -0,0 +1,69 @@
+Guidelime.registerGuide(
+[[
+[N6-12 Durotar]
+[GA Orc,Troll]
+[D A guide created by |cFF69CCF0Ettern|r\\http://www.dreamstate.gg]
+[G54.3,73.4 Durotar]Accept [QA786 Thwarting Kolkar Aggression](NPC patrols around).
+In Sen'jin Village accept the following quests: \\[QA817 Practical Prey][G56,73.9] (inside the hut) \\[QA818 A solvent Spirit][G55.9,74.4].
+Turn in [QT805 Accept Report to Sen'jin Village][G56,74.7]. \\Accept [QA808 Minshina's Skull] \\Accept [QA823 Report to Orgnil] \\Accept [QA826 Zalazane].
+[G56.3,75.1]|cFF69CCF0[T]Learn new skills.[A Mage]\\ - Fireball\\ - Conjure Food\\ - Fire Blast|r
+Learn professions (optional): \\ - [T]Alchemy[G55.4,74] \\ - [T]Herbalism[G55.4,75.1][O]
+Run up north to Razor Hill. \\Turn in [QC823 Report to Orgnil][G52.2,43.1] \\Accept [QA806 Dark Storms]. 
+[G52,43.4]In the burrow next to you, accept [QA784 Vanquish the Betrayers] and [QA837 Encroachment].
+[G51.1,42.4]Accept [QA815 Break a Few Eggs]
+[G49.9,40.4]Go up to the top of the watchtower NW of Razor Hill and accept [QA791 Carry Your Weight].
+[G54.4,41.19]|cFF9482C9[T]Learn new skills.[A Warlock] \\ - Life Tap (Rank 1) \\ - Shadow Bolt (Rank 2)
+[G51.5,41.6]Go in the Inn next to you and turn in [QT2161 A Peon's Burden]. \\[S]Set hearthstone] \\[V]Sell items \\[V]Buy level 5 water/food (you need 1 silver for First Aid).
+Enter the Barracks, make a left at the entrance and get learn [T]First Aid from Rawrk[G54.2,41.9].
+Learn professions (optional): \\ - [T]Mining, Blacsmithing and Engineering[G52,40.8]
+Go to Tiragarde Keep and do [QC784 Vanquish the Betrayers][O] along with [QC791 Carry Your Weight][O][G58.17,57.13]: \\ - Kill the humans and work your way to the top of the fortress.
+Kill Lieutenant Benedict[G59.73,58.28] for [QC784,2 Vanquish the Betrayers]. \\- Loot his key.
+Go up to Benedict's Chest [G59.32,57.65], open it and accept [QA830 The Admiral's Orders (part 1)] from the Aged Envelope inside.
+Jump down and finish: \\[QC784 Vanquish the Betrayers] \\[QC791 Carry Your Weight].
+Grind your way back to  Razor Hill or die/unstuck.[O]
+[G52,43.4]Turn in [QC784 Vanquish the Betrayers] and [QC830 The Admiral's Orders (part 1)]. \\Accept [QA825 From The Wreckage....] \\Accept [QA831 The Admiral's Orders (part 2)]
+[G49.9,40.4]Go up to the top of the watchtower, and turn in [QT791 Carry Your Weight].
+[G61.79,42.38]Go east to the beach and in the water do [QC818 A solvent Spirit] along with [QC825 From The Wreckage....]: \\ - Kill/loot the crabs and makrura mobs and loot the Gnomish Toolboxes.
+Go just SW of Razor Hill and do the first half of [QC837 Encroachment][OC] [G50.02,49.37]: \\ - Kill Razormane Quilboars and Razormane Scouts.
+[G52,43.4]Go back to Razor Hill and turn in [QT825 From The Wreckage....].
+Grind your way south and do [QC786 Thwarting Kolkar Aggression]: \\ - [QC786,1 Attack Plan: Valley of Trials][G49.81,81.31] \\ - [QC786,2 Attack Plan: Sen'jin Village][G47.67,77.39] \\ - [QC786,3 Attack Plan: Orgrimmar][G46.26,78.98]
+Grind mobs here until level [XP8]
+Learn profession (optional): \\ - [T]Fishing[G53.3,81.6][O]
+Go to Sen'jin Village and turn in [QT786 Thwarting Kolkar Aggression][G54.3,73.4] (NPC patrols)
+Turn in [QT818 A solvent Spirit][G55.9,74.4].
+Keep the reward for later use|cFF00FF96 Really Sticky Glue|r.[O]
+[G55.6,73.6][R]Repair and [S]sell items.
+[T]Learn new skills: [A Mage] |cFF69CCF0 \\ - Polymorph \\ - Frostbolt |r
+Go to Echo Isles and do the following 4 quests together: \\[QC815 Break a Few Eggs] - Pickup Taillasher Eggs\\[QC817 Practical Prey] - Kill Durotar Tigers\\[QC808 Minshina's Skull][G67.4,87.8] - On the little hill with candles around it\\[QC826 Zalazane][G67.19,86.98] - Kill Trolls and Zalazane.
+Die/unstuck to get to Sen'jin Village.
+Turn in the following quests in Senjin Village: \\[QT817 Practical Prey][G56,73.9]\\[QT808 Minshina's Skull][G56,74.7] and [QT826 Zalazane][G56,74.7]. 
+Keep the reward for later use|cFF00FF96 Faintly Glowing Skull|r.[O]
+Grind your way back to Razor Hill[O]
+[G51.1,42.4]Turn in [QT815 Break a Few Eggs].
+[G51.91,41.14][R]Repair/Sell items[O]
+[G51.5,41.6][V]Buy level 5 water/food[O]
+[G43.98,40.19]Go west of Razor Hill and do the second half of [QC837 Encroachment]: \\ - Kill the Razormane Dustrunners and Razormane Battleguards.
+[G43.09,30.34]Grind your way north to the little house and accept [QA816 Lost But Not Forgotten].
+[G46.37,23.12]Go NE to the goblin and accept [QA834 Winds in the Desert].
+Do [QC834 Winds in the Desert]: loot Stolen Supply Sacks around the harpies. \\ - (See icon[L47.2,29.61][L47.18,30.87][L48.13,34.31][L49.21,32.99][L49.66,32.11] on map for locations)
+[G46.37,23.12]Go back to the goblin and turn in [QC834 Winds in the Desert].\\Accept [QA835 Securing the Lines].
+[G41.53,18.6]Go NW and between the large rocks accept [QA812 Need for a Cure]. \\NOTE: Don't worry about the timer countdown.
+Keep grinding up north (near Orgrimmar's entrance) until you reach level [XP10].
+[G49.52,69.34 Orgrimmar]Go north into Orgrimmar and visit bank. \\Stash in bank: |cFF00FF96Really Sticky Glue|r [O]
+Visit Auction House and look for the following items: \\ - Wand/weapon \\ - Healing/mana potions \\ - Armor \\ - Rings with intellect (+ spirit).[A Mage][OC]
+Visit Auction House and look for the following items: \\ - Wand/weapon \\ - Healing/mana potions \\ - Armor \\ - Rings.[A Warlock][OC]
+Visit Auction House and look for the following items: \\ - Wand/weapon \\ - Mana potions \\ - Armor \\ - Rings[A Priest][OC]
+Visit Auction House and look for the following items: \\ - Weapon \\ - Healingpotions \\ - Armor \\ - Rings[A Warrior][OC]
+Visit Auction House and look for the following items: \\ - Weapon \\ - Healing/mana potions \\ - Armor \\ - Rings[A Shaman][OC]
+Visit Auction House and look for the following items: \\ - Weapon \\ - Healing/mana potions \\ - Armor \\ - Rings[A Hunter][OC]
+Visit Auction House and look for the following items: \\ - Weapon \\ - Healing potions \\ - Armor \\ - Rings[A Rogue][OC]
+[G38.89,85.57 Orgrimmar]Visit Mage Trainer (38.87) and learn new skills.[T].
+|cFFFF0000 Don't accept the mage quest.[A Mage][O]|r
+[G46.98,53.71 Orgrimmar]Go into Cleft of Shadow and accept [QA813 Finding the Antidote].
+Learn professions (optional): \\ - [T]Tailoring[G62.9,49.2 Orgrimmar] \\ - [T]Skinning and leatherworking[G63.21,45.38Orgrimmar] \\ - [T]Enchanting[G53.6,38.17 Orgrimmar] \\ - [T]Alchemy[G56.45,33.85 Orgrimmar] \\ - [T]Mining[G73.12,26.24 Orgrimmar] \\ - [T]Blacksmithing[G82.13,23.23 Orgrimmar] \\ - [T]Engineering[G76.01,24.83Orgrimmar] \\ - [T]Herbalism[G55.69,40.05Orgrimmar]
+[G31.81,37.78Orgrimmar]Go to Thrall and accept [QA5726 Hidden Enemies (part 1)].
+[G32.2,35.78Orgrimmar]Next to you, turn in [QT831 The Admiral's Orders].
+Hearth to [H Razor Hill].[O]
+[NX 12-14 Barrens]
+
+]], "Ettern leveling guide")
